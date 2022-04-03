1eae3a3a4755d6882a5cecfb43217e810a73309a
loritobad <kike_mto@hotmail.com>
2022-04-03 22:45:52 +0200

    modulo clientes

--- app/clients/routes.py
+++ app/clients/routes.py
@@ -1,10 +1,20 @@
-from flask import render_template
+from flask import redirect, render_template, request, url_for
+from app.clients.forms import LoginClientes
 from . import  clients
-from forms import LoginClientes
 # vistas de dominio público
 
-@clients.route('/clients')
-def index():
-    return render_template('login.html')
+@clients.route('/clients',  methods=["GET", "POST"])
+def login():
+    form = LoginClientes()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("login.html", form=form)
 
+#https://j2logo.com/tutorial-flask-leccion-3-formularios-wtforms/
+#https://bootsnipp.com/snippets/z8aQr
 
